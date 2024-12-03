9b226353906bb4178625902a423193838a0787cf
Felix Hillingshaeuser <felix@mxcore.de>
2024-12-03 07:59:28 +0100

    add day03 command

---
+++ cmd/day03.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/unixlab/AoC2024/internal/aoeinput"
+	"github.com/unixlab/AoC2024/internal/day03"
+)
+
+// day03Cmd represents the day03 command
+var day03Cmd = &cobra.Command{
+	Use: "day03",
+	Run: func(cmd *cobra.Command, _ []string) {
+		example, err := cmd.Flags().GetBool("example")
+		if err != nil {
+			panic(err)
+		}
+		day := cmd.Use
+		if example {
+			day += "p1"
+		}
+		input := aoeinput.Read("", day, example)
+		fmt.Printf("day03 part 1 => %d\n", day03.RunPart1(input))
+		if example {
+			day = cmd.Use + "p2"
+		}
+		input = aoeinput.Read("", day, example)
+		fmt.Printf("day03 part 2 => %d\n", day03.RunPart2(input))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(day03Cmd)
+}
