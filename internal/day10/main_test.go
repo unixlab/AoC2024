394a6ee2ebb0c30fcf25c7df13e245afe85cc046
Felix Hillingshaeuser <felix@mxcore.de>
2024-12-10 11:33:12 +0100

    add day10 tests

---
+++ internal/day10/main_test.go
@@ -0,0 +1,39 @@
+package day10
+
+import (
+	"testing"
+
+	"github.com/unixlab/AoC2024/internal/aoeinput"
+)
+
+func TestRunPart1Example(t *testing.T) {
+	expectedResult := 36
+	actualResult := RunPart1(aoeinput.Read("../../", "day10", true))
+	if actualResult != expectedResult {
+		t.Fatalf("RunPart1() = expected %v, got %v", expectedResult, actualResult)
+	}
+}
+
+func TestRunPart1(t *testing.T) {
+	expectedResult := 786
+	actualResult := RunPart1(aoeinput.Read("../../", "day10", false))
+	if actualResult != expectedResult {
+		t.Fatalf("RunPart1() = expected %v, got %v", expectedResult, actualResult)
+	}
+}
+
+func TestRunPart2Example(t *testing.T) {
+	expectedResult := 81
+	actualResult := RunPart2(aoeinput.Read("../../", "day10", true))
+	if actualResult != expectedResult {
+		t.Fatalf("RunPart2() = expected %v, got %v", expectedResult, actualResult)
+	}
+}
+
+func TestRunPart2(t *testing.T) {
+	expectedResult := 1722
+	actualResult := RunPart2(aoeinput.Read("../../", "day10", false))
+	if actualResult != expectedResult {
+		t.Fatalf("RunPart2() = expected %v, got %v", expectedResult, actualResult)
+	}
+}
