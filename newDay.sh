b2efa0301cac745aaf123c0c5eee22664eee30df
Felix Hillingshaeuser <felix@mxcore.de>
2025-08-23 12:49:40 +0200

    make newDay.sh more platform independent

--- newDay.sh
+++ newDay.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -x
 
+if [[ "$(uname -s)" == "Darwin" ]]
+then
+	SED="gsed"
+else
+	SED="sed"
+fi
+
 lastDay=$(find cmd -name "day*.go" | grep -v XX | sort -n | tail -1 | sed 's#cmd/day##' | sed 's/.go//g')
 nextDay=$((10#$lastDay+1))
 nextDayLeadingZero=$(printf "day%02d" $nextDay)
@@ -7,7 +14,7 @@ nextDayLeadingZero=$(printf "day%02d" $nextDay)
 cp examples/dayXX examples/${nextDayLeadingZero}
 cp inputs/dayXX inputs/${nextDayLeadingZero}
 cp cmd/dayXX.go cmd/${nextDayLeadingZero}.go
-sed -i "s/dayXX/${nextDayLeadingZero}/g" cmd/${nextDayLeadingZero}.go
+$SED -i "s/dayXX/${nextDayLeadingZero}/g" cmd/${nextDayLeadingZero}.go
 cp -r internal/dayXX internal/${nextDayLeadingZero}
-sed -i "s/dayXX is our template package/${nextDayLeadingZero} is our package for the ${nextDay}th AoC day/g" internal/${nextDayLeadingZero}/*.go
-sed -i "s/dayXX/${nextDayLeadingZero}/g" internal/${nextDayLeadingZero}/*.go
+$SED -i "s/dayXX is our template package/${nextDayLeadingZero} is our package for the ${nextDay}th AoC day/g" internal/${nextDayLeadingZero}/*.go
+$SED -i "s/dayXX/${nextDayLeadingZero}/g" internal/${nextDayLeadingZero}/*.go
