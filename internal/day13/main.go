a3c59b16116df9686d9eabd12b01f208d905ab7b
Felix Hillingshaeuser <felix@mxcore.de>
2025-08-23 17:03:31 +0200

    add day13 part 1 code

---
+++ internal/day13/main.go
@@ -0,0 +1,72 @@
+// Package day13 is our package for the 13th AoC day
+package day13
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// RunPart1 is for the first star of the day
+func RunPart1(input []string) int {
+	part1Sum := 0
+	var aX, aY, bX, bY, pX, pY float64
+	for _, line := range input {
+		if strings.HasPrefix(line, "Button A") {
+			re := regexp.MustCompile(`X\+(\d+), Y\+(\d+)`)
+			matches := re.FindStringSubmatch(line)
+			aX, _ = strconv.ParseFloat(matches[1], 64)
+			aY, _ = strconv.ParseFloat(matches[2], 64)
+		}
+		if strings.HasPrefix(line, "Button B") {
+			re := regexp.MustCompile(`X\+(\d+), Y\+(\d+)`)
+			matches := re.FindStringSubmatch(line)
+			bX, _ = strconv.ParseFloat(matches[1], 64)
+			bY, _ = strconv.ParseFloat(matches[2], 64)
+		}
+		if strings.HasPrefix(line, "Prize") {
+			re := regexp.MustCompile(`X=(\d+), Y=(\d+)`)
+			matches := re.FindStringSubmatch(line)
+			pX, _ = strconv.ParseFloat(matches[1], 64)
+			pY, _ = strconv.ParseFloat(matches[2], 64)
+		}
+		if pX != 0 {
+			// after some googling I found we can use Cramer's rule
+			// https://en.wikipedia.org/wiki/Cramer%27s_rule#2x2_System
+			d := aX*bY - aY*bX
+			if d != 0 {
+				a := (pX*bY - pY*bX) / d
+				b := (aX*pY - aY*pX) / d
+				aX, aY, bX, bY, pX, pY = 0, 0, 0, 0, 0, 0
+				// boundary check a between 0 and 100
+				if a <= 0 || a > 100 {
+					continue
+				}
+				// boundary check b between 0 and 100
+				if b <= 0 || b > 100 {
+					continue
+				}
+				// check if a is not a whole number
+				if a != float64(int(a)) {
+					continue
+				}
+				// check if b is not a whole number
+				if b != float64(int(b)) {
+					continue
+				}
+				part1Sum += int(3*a + b)
+			}
+			aX, aY, bX, bY, pX, pY = 0, 0, 0, 0, 0, 0
+		}
+	}
+	return part1Sum
+}
+
+// RunPart2 is for the second star of the day
+func RunPart2(input []string) int {
+	for _, line := range input {
+		fmt.Println(line)
+	}
+	return 0
+}
---
+++ internal/day13/main_test.go
@@ -0,0 +1,39 @@
+package day13
+
+import (
+	"testing"
+
+	"github.com/unixlab/AoC2024/internal/aocinput"
+)
+
+func TestRunPart1Example(t *testing.T) {
+	expectedResult := 480
+	actualResult := RunPart1(aocinput.Read("../../", "day13", true))
+	if actualResult != expectedResult {
+		t.Fatalf("RunPart1() = expected %v, got %v", expectedResult, actualResult)
+	}
+}
+
+func TestRunPart1(t *testing.T) {
+	expectedResult := 28262
+	actualResult := RunPart1(aocinput.Read("../../", "day13", false))
+	if actualResult != expectedResult {
+		t.Fatalf("RunPart1() = expected %v, got %v", expectedResult, actualResult)
+	}
+}
+
+func TestRunPart2Example(t *testing.T) {
+	expectedResult := 0
+	actualResult := RunPart2(aocinput.Read("../../", "day13", true))
+	if actualResult != expectedResult {
+		t.Fatalf("RunPart2() = expected %v, got %v", expectedResult, actualResult)
+	}
+}
+
+func TestRunPart2(t *testing.T) {
+	expectedResult := 0
+	actualResult := RunPart2(aocinput.Read("../../", "day13", false))
+	if actualResult != expectedResult {
+		t.Fatalf("RunPart2() = expected %v, got %v", expectedResult, actualResult)
+	}
+}
